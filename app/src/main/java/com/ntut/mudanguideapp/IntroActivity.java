6f47f15604f17a2d5782a9553bc8e7dc8c629f96
106820026 <i8807701314520@yahoo.com.tw>
2018-05-03 18:05:17 +0800

    Add line

--- app/src/main/java/com/ntut/mudanguideapp/IntroActivity.java
+++ app/src/main/java/com/ntut/mudanguideapp/IntroActivity.java
@@ -45,7 +45,7 @@ public class IntroActivity extends AppCompatActivity {
         webContainer = findViewById(R.id.intro_web);
         webView = new WebView(this);
         //webView.loadUrl("file:///android_asset/HTML/"+intent.getStringExtra("html"));
-        webView.loadUrl("file:///android_asset/HTML/FourforestsVillageIntro.html");
+        webView.loadUrl("file:///android_asset/HTML/MudanTownIntro.html");
         webContainer.addView(webView);
     }
 
